d7d8ba5fbf45b3efaa02af7c261a4eec3859bb22
Gustiagung19 <75777108+Gustiagung19@users.noreply.github.com>
2021-04-13 23:08:12 +0800

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,6 @@
 AddEventHandler('chatMessage',
 function(source, name, message)
-  PerformHttpRequest('MASUKKAN DISCORD KLEEEAN DISINI',
+  PerformHttpRequest('MASUKKAN DISCORD WEBHOOK KLEEEAN DISINI',
   function(err, text, headers)
   end,
   'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
