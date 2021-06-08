79e9d13b4020f0da1fbec68d887ff322764f537d
Gustiagung19 <75777108+Gustiagung19@users.noreply.github.com>
2021-06-09 00:24:24 +0800

    Delete server.lua

--- server.lua
+++
@@ -1,7 +0,0 @@
-AddEventHandler('chatMessage',
-function(source, name, message)
-  PerformHttpRequest('MASUKKAN DISCORD WEBHOOK KLEEEAN DISINI',
-  function(err, text, headers)
-  end,
-  'POST', json.encode({username = name, content = message}), { ['Content-Type'] = 'application/json' })
-end)
